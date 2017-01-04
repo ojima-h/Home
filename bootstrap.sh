dc5b50e81e7c2d90d95d12c9d30393e352d2a52f
Ojima Hikaru <amijo4rihaku@gmail.com>
2017-01-04 14:53:37 +0900

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,15 +11,15 @@ then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-# Clone Home.git
-if [ ! -d $WORKDIR/Home ]
-then
-  git clone git@github.com:ojima-h/Home.git $WORKDIR/Home
-fi
-
 # Install ansible
 if [ ! -x /usr/local/bin/ansible ]
 then
   sudo easy_install pip
   sudo pip install ansible
+fi
+
+# Clone Home.git
+if [ ! -d $WORKDIR/Home ]
+then
+  git clone git@github.com:ojima-h/Home.git $WORKDIR/Home
 fi
