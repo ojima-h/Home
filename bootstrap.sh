00e8dc0a682d7925a261f7d1b0b12dca17fcc7c3
Hikaru Ojima <ojima.h@gmail.com>
2017-01-04 14:34:15 +0900

    use ansible

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -eu
+
 mkdir -p ~/Workspace
 
 # Install Homebrew
@@ -14,18 +16,9 @@ then
   git clone git@github.com:ojima-h/Home.git ~/Workspace/Home
 fi
 
-# Install anyenv 
-if [ ! -d ~/.anyenv ]
-then
-  git clone https://github.com/riywo/anyenv ~/.anyenv
-  echo 'export PATH="$HOME/.anyenv/bin:$PATH"' >> ~/.bash_profile
-  echo 'eval "$(anyenv init -)"' >> ~/.bash_profile
-fi
-
-# Install Python
-bash -lc 'anyenv install pyenv -p'
-bash -lc 'pyenv install 3.5.2 -p'
-cd Workspace && pip local 3.5.2
-
 # Install ansible
-bash -lc 'PYENV_VERSION=3.5.2 pip install ansible'
+if [ ! -x /usr/local/bin/ansible ]
+then
+  sudo easy_install pip
+  sudo pip install ansible
+fi
